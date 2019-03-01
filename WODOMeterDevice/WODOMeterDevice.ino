0e6fe0e5688182740975f5cc6cee826cb1457f57
Jeff Van Nortwick <jvannor@hotmail.com>
2019-02-28 20:08:25 -0500

    Fixed service and characteristic ids

--- WODOMeterDevice/WODOMeterDevice.ino
+++ WODOMeterDevice/WODOMeterDevice.ino
@@ -3,9 +3,12 @@
 #define SERVICE_ID 0x6C76DEFEA4654437827601146E567164
 #define CHARACTERISTIC_ID 0x9CAAF395F500451EB47E21761EDC1290
 
+uint8_t svc_id[] = {0x64, 0x71, 0x56, 0x6E, 0x14, 0x01, 0x76, 0x82, 0x37, 0x44, 0x65, 0xA4, 0xFE, 0xDE, 0x76, 0x6C};
+uint8_t chr_id[] = {0x90, 0x12, 0xDC, 0x1E, 0x76, 0x21, 0x7E, 0xB4, 0x1E, 0x45, 0x00, 0xF5, 0x95, 0xF3, 0xAA, 0x9C};
+
 BLEDis bledis;
-BLEService wodometer = BLEService(SERVICE_ID);
-BLECharacteristic measurement = BLECharacteristic(CHARACTERISTIC_ID);
+BLEService wodometer = BLEService(svc_id);
+BLECharacteristic measurement = BLECharacteristic(chr_id);
 
 uint32_t value = 0;
 
@@ -31,9 +34,9 @@ void setup() {
 
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-  Bluefruit.Advertising.addName();
   Bluefruit.Advertising.addService(wodometer);
-
+  Bluefruit.Advertising.addName();
+  
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);
   Bluefruit.Advertising.setFastTimeout(30);
